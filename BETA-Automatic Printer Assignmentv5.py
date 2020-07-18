a1487f4e525605a3a7f468fb1217e2c62cc61dd8
x3xplosiiv3x <x3xplosiiv3x@gmail.com>
2020-07-17 22:28:38 -0400

    added more of a description

--- BETA-Automatic Printer Assignmentv5.py
+++ BETA-Automatic Printer Assignmentv5.py
@@ -1,6 +1,7 @@
 #This is a program that looks for printers on your computer and allows the user to choose the default printer.
 #Created by: Brandon Ferrotta
 #This is a beta, trying to turn this into a exe file
+#no clue why pyinstaller doesnt work properly
 import subprocess, re, time, sys, os
 from colorama import Fore, Back, Style
 def main():
